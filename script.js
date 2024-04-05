b558df119c3cd26ca16650acd9daeb4c2b86138e
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-04-05 16:11:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+
 function insert_Row() {
-    //Write your code here
-  
-  
+    let table=document.querySelector("#sampleTable");
+	let td1=document.createElement("td");
+	let td2=document.createElement("td");
+	td1.textContent='New Cell1';
+	td2.textContent='New Cell2';
+	let tr=table.insertRow(0);
+	tr.appendChild(td1);
+	tr.appendChild(td2);
 }
+
